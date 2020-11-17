d3111a0ddfd5a75a229c7015245740c096db86f1
reshma <reshmashankar@gmail.com>
2020-11-17 22:05:57 +0530

    test class error

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/DemoApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/DemoApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.runner.RunWith;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
