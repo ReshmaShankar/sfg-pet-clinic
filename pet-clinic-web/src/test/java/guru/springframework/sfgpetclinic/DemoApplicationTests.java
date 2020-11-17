a592430627519ddd7310d062f365cbdbd4305b4d
reshma <reshmashankar@gmail.com>
2020-11-17 21:54:57 +0530

    Test class changes

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/DemoApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/DemoApplicationTests.java
@@ -2,7 +2,10 @@ package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
